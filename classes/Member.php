1d3ce166223fe35bfadcbf098d33bbf0637d909f
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-05-05 22:34:17 -0400

    View update and Member classes added.

---
+++ classes/Member.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saman
+ * Date: 5/5/2019
+ * Time: 7:31 PM
+ */
+
+class Member
+{
+    protected $fname, $lname, $age, $gender, $phone, $email, $state, $seeking, $bio, $img,
+        $isPremium = false;
+
+
+    public function __construct($fname, $lname, $age, $gender, $phone)
+    {
+        $this->fname = $fname;
+        $this->lname = $lname;
+        $this->age = $age;
+        $this->gender = $gender;
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $state
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $img
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIsPremium()
+    {
+        return $this->isPremium;
+    }
+
+    /**
+     * @param mixed $isPremium
+     */
+    public function setIsPremium($isPremium)
+    {
+        $this->isPremium = $isPremium;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * @param mixed $fname
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * @param mixed $lname
+     */
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSeeking()
+    {
+        return $this->seeking;
+    }
+
+    /**
+     * @param mixed $seeking
+     */
+    public function setSeeking($seeking)
+    {
+        $this->seeking = $seeking;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBio()
+    {
+        return $this->bio;
+    }
+
+    /**
+     * @param mixed $bio
+     */
+    public function setBio($bio)
+    {
+        $this->bio = $bio;
+    }
+
+
+}
