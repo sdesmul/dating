d78079b6731adc5f229a85d20a1ade1d647f8e00
Samantha DeSmul <samanthadesmul@yahoo.com>
2019-05-27 23:36:55 -0400

    Updated views for admin

---
+++ model/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saman
+ * Date: 5/27/2019
+ * Time: 10:48 AM
+ */
+
+define("DB_DSN", "mysql:dbname=sdesmulc__it328");
+define("DB_USERNAME", "sdesmulc_students");
+define("DB_PASSWORD", "grc_user!");
